12204ede3d03693f1ef1c1803b8dc63c09b8002b
C2Meng <chancm530@gmail.com>
2026-04-23 02:44:12 +0800

    added and testing frontend design mockups

--- numero/core/views.py
+++ numero/core/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 #difference of render vs loader.get_template is that render is a shortcut that combines loading the template and rendering it in one step, while loader.get_template only loads the template and returns a Template object that can be rendered later. In this code, we are using render to load the 'test.html' template and then rendering it with an empty context using render() before returning the HttpResponse.
 def test(request):
-    return render(request, 'test.html')
+    return render(request, 'hobbies/calendar.html')
