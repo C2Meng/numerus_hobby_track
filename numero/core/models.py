73d2c52e33ec1b707a81ffe6772ef2b070ff2a9c
C2Meng <chancm530@gmail.com>
2026-04-23 02:11:10 +0800

    added Hobbies and schedule to model

--- numero/core/models.py
+++ numero/core/models.py
@@ -3,5 +3,24 @@ from django.db import models
 from django.db import models
 
 class Hobbies(models.Model):
-  hobby = models.CharField(max_length=255)
+  id = models.IntegerField(primary_key=True)
+  hobby_name = models.CharField(max_length=255)
+  priority =models.IntegerField()
+  weekly_hours = models.IntegerField()
   description = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+
+class Schedule(models.Model):
+  id = models.IntegerField(primary_key=True)
+  day_of_week = models.CharField(max_length=255)
+  start_time = models.TimeField()
+  end_time = models.TimeField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  
+    
+    
+   
